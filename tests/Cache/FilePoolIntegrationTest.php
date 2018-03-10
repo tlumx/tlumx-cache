96586e0bfecff7cf6913e59b4af3fd212947385d
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-03-10 23:24:33 +0200

    .

--- tests/Cache/FilePoolIntegrationTest.php
+++ tests/Cache/FilePoolIntegrationTest.php
@@ -32,7 +32,9 @@ class FilePoolIntegrationTest extends CachePoolTest
             }
         }
         $options = [
-            'directory' => $this->cacheDir
+            'directory' => $this->cacheDir,
+            'directory_level' => 2,
+            'file_suffix' => '.cache'
         ];
 
         return new FileCachePool($options);
@@ -43,5 +45,67 @@ class FilePoolIntegrationTest extends CachePoolTest
         parent::tearDown();
         testRemoveDirTree($this->cacheDir);
         unset($this->cacheDir);
+    }
+
+    public function testConstructNotDirectoryOption()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $pool = new FileCachePool([]);
+    }
+
+    public function testConstructNotIsDirectory()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $pool = new FileCachePool([
+            'directory' => ''
+        ]);
+    }
+
+    public function testConstructNotWritebleDirectory()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $pool = new FileCachePool([
+            'directory' => '/home'
+        ]);
+    }
+
+    public function testIsHavDataInStorageFalseTime()
+    {
+        $pool = $this->createCachePool();
+        $item = $pool->getItem('key');
+        $item->set('value');
+        $item->expiresAfter(1);
+        $pool->save($item);
+        sleep(3);
+        $this->assertFalse($pool->hasItem('key'));
+    }
+
+    public function testPrefix()
+    {
+        $pool = $this->createCachePool();
+        $this->assertEquals('tlumxframework_', $pool->getPrefix());
+        $pool->setPrefix('tlumxframework_tmp_cache1');
+        $this->assertEquals('tlumxframework_tmp_cache1', $pool->getPrefix());
+    }
+
+    public function testTtl()
+    {
+        $pool = $this->createCachePool();
+        $this->assertEquals(3600, $pool->getTtl());
+        $pool->setTtl(300);
+        $this->assertEquals(300, $pool->getTtl());
+    }
+
+    public function testGetItemsDeferredSave()
+    {
+        $pool = $this->createCachePool();
+        $item = $pool->getItem('key');
+        $item->set('4711');
+        $return = $pool->saveDeferred($item);
+        $this->assertTrue($return);
+
+        $items = $pool->getItems(['key']);
+        $item1 = $items['key'];
+        $this->assertEquals('4711', $item1->get());
     }
 }
